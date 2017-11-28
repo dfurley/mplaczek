c8af000c5a9c2b425acca0b17d479463785d2da4
= <=>
2017-11-28 16:15:21 +0000

    added bootstrap javascript

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-md">
+<nav class="navbar navbar-expand-md navbar-dark">
   <div class="container">
   	<div class="col-md-10 offset-md-1">
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
 		  <div class="collapse navbar-collapse" id="navbarNav">
-		    <ul class="navbar-nav">
+		    <ul class="navbar-nav mr-auto">
 		      <li class="nav-item">
 		        <a class="nav-link" href="/">Home</a>
 		      </li>
@@ -23,3 +23,5 @@
 		</div>
   </div>
 </nav>
+
+
