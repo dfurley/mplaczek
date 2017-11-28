74cb71f19e3718f507f464e6a7bfcc526bea8441
= <=>
2017-11-28 15:10:03 +0000

    updated background styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
 	<!-- Bootstrap Core CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	<!-- Custom font -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400" rel="stylesheet">
 	<!-- CSS -->
 	<link rel="stylesheet" href="/css/app.css">
 </head>
--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,5 +1,10 @@
-<div class="container">
-	<div class="col-md-10 offset-md-1">
-		<h1>Placzek Lab</h1>
+<div class="banner-bg">
+	<div class="container">
+		<div class="col-md-10 offset-md-1">
+			<div class="row">
+				<img class ="banner-img" src="https://image.ibb.co/kC8wV6/sheffield_logo_clear_background.png" alt="sheffield_logo_clear_background" border="0">
+				<div class="banner">Placzek Lab</div>
+			</div>
+		</div>
 	</div>
 </div>
