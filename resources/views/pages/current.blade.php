879411daffc3f6f48f3758731a39d7763733646c
= <=>
2017-11-29 11:22:56 +0000

    more banner stuff

--- resources/views/pages/current.blade.php
+++ resources/views/pages/current.blade.php
@@ -1,7 +1,18 @@
 @extends('layout')
 
+<link rel="stylesheet" href="/css/people.css">
+
+@section ('image')
+	<div class="banner">
+		<div class="img-container">
+			<div class="inner-container">
+				<h1>current lab members</h1>
+			</div>
+		</div>
+	</div>
+@endsection
 @section ('content')
-	
+	<div class="content">
 		<h2>People</h2>
-	
+	</div>
 @endsection
